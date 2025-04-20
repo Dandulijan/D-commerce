0bccbbb25ef5ed105128ab579cf226c11ee29d27
dandulijan <daldulijan@hotmail.com>
2025-04-20 12:59:58 +0300

    initialize' git commit -m initialize

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'Profile', component: ProfileComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailsComponent },
+  { path: '**', redirectTo: 'dashboard' },
+];
